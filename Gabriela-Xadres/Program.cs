6dcb51fc540bff33773f464f3127cb89644d770b
gabrielabvitorino <gabrielabvitorino@gmail.com>
2017-11-29 15:04:19 -0200

    comecando a implementar um metodo que imprime tabuleiro

--- Gabriela-Xadres/Program.cs
+++ Gabriela-Xadres/Program.cs
@@ -5,13 +5,9 @@ namespace Gabriela_Xadres {
     class Program {
         static void Main(string[] args) {
 
-            Posicao p;
-            
-            p = new Posicao(3, 4);
-            
-            Console.WriteLine("Posição: " + p);
-
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
         }
