e5e0d75665223d18fba554cdac827f1799eb3243
gabrielabvitorino <gabrielabvitorino@gmail.com>
2017-11-29 10:06:48 -0200

    criando classe posicao

---
+++ Gabriela-Xadres/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace Gabriela_Xadres {
+    class Program {
+        static void Main(string[] args) {
+
+            posicao p;
+
+            p = new posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+
+            Console.ReadLine();
+        }
+    }
+}
