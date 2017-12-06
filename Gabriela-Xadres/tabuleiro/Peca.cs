baa6a1f665887205fef816849bda9ea0364703ec
gabrielabvitorino <gabrielabvitorino@gmail.com>
2017-12-06 12:23:09 -0200

    Validando a posição destino

--- Gabriela-Xadres/tabuleiro/Peca.cs
+++ Gabriela-Xadres/tabuleiro/Peca.cs
@@ -13,6 +13,33 @@ namespace tabuleiro {
              this.cor = cor;
              this.qteMovimentos = 0;
          }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i=0; i<tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis(pos)[pos.linha, pos.coluna];
+        }
+
         public abstract bool[,] movimentosPossiveis();
 }
  }
