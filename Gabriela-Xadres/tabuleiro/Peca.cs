73db72488d8b6a743b01e9244e27663adff5d0bd
gabrielabvitorino <gabrielabvitorino@gmail.com>
2017-12-06 11:07:21 -0200

    Testes iniciais com movimentos possiveis das pecas

--- Gabriela-Xadres/tabuleiro/Peca.cs
+++ Gabriela-Xadres/tabuleiro/Peca.cs
@@ -1,6 +1,6 @@
 ﻿
 namespace tabuleiro {
-     class Peca
+     abstract class Peca
      {
          public Posicao posicao { get; set; }
          public Cor cor { get; protected set; }
@@ -13,8 +13,6 @@ namespace tabuleiro {
              this.cor = cor;
              this.qteMovimentos = 0;
          }
-        public void incrementarQteMovimentos() {
-             qteMovimentos++;
-         }
+        public abstract bool[,] movimentosPossiveis();
 }
  }
